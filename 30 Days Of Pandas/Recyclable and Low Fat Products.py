432bb92bd8f86bda8b537a25cc8ff2ccd9964489
Akhila Angara <95868731+akhila-haritha@users.noreply.github.com>
2023-10-23 10:28:50 +0530

    Create Recyclable and Low Fat Products.py

---
+++ 30 Days Of Pandas/Recyclable and Low Fat Products.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    items = products[((products['low_fats'] == 'Y') & (products['recyclable']=='Y'))]['product_id']
+    items = pd.DataFrame(items)
+
+    return items
+    
