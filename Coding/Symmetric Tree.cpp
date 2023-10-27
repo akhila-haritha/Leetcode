633feb3829b689759ea5f7f2e1184e21e7dcd4b6
Akhila Angara <95868731+akhila-haritha@users.noreply.github.com>
2023-10-27 20:16:48 +0530

    Create Symmetric Tree.cpp

---
+++ Coding/Symmetric Tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSym(TreeNode *p, TreeNode *q) {
+        if (!p && !q) return true; 
+        if (!p || !q) return false; 
+        if (p->val != q->val) return false; 
+
+        
+        return isSym(p->left, q->right) && isSym(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true; 
+        return isSym(root->left, root->right);
+    }
+};
